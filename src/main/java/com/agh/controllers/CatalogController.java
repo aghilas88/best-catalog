525b250a1ff87d5dbd514fa191d2ef3cc6468fca
aghilas <aghilasdjm@gmail.com>
2018-01-17 17:25:13 +0100

    # WARNING: head commit changed in the meantime
    add catalog service

--- src/main/java/com/agh/controllers/CatalogController.java
+++ src/main/java/com/agh/controllers/CatalogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.agh.domain.*;
 import com.agh.services.*;
 
-@RestController @RequestMapping(value ="/catalogs") 
+//@RestController @RequestMapping(value ="/catalogs") 
 public class CatalogController {
 	
 	@Resource
@@ -23,53 +23,53 @@ public class CatalogController {
 //	@Resource
 //	private ISkuService SkuService;
 
-	/**
-	 * @return all catalogs
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public Collection<Catalog> getAllCatalogs() {
- 	  	return this.catalogService.getAllCatalogs();	
-	}
-	
-	/**
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Catalog getCatalogById(@PathVariable(value = "id") Long id) {
-   		return this.catalogService.getCatalogById(id);
-	}
-	
-	/**
-	 * create catalog
-	 * @param catalog
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public Catalog createCatalog(@RequestBody Catalog catalog) {
- 		return this.catalogService.createCatalog(catalog);	
-	}
-	
-	/**
-	 * @param id
-	 * @param catalog
-	 * @return
-	 */
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public Catalog updateCatalog(@PathVariable(value = "id") Long id, @RequestBody Catalog catalog) {
-		catalog.setId(id);
- 		return this.catalogService.updateCatalog(catalog);
- 	}
-	
-	/**
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/{id}/categories", method = RequestMethod.GET)
-	public Collection<Category> getCategoriesByCatalogId(@PathVariable(value = "id") Long id) {
-		Catalog catalog = this.catalogService.getCatalogById(id);
-		return catalog.getRootCategories();
-	}
+//	/**
+//	 * @return all catalogs
+//	 */
+//	@RequestMapping(method = RequestMethod.GET)
+//	public Collection<Catalog> getAllCatalogs() {
+// 	  	return this.catalogService.getAllCatalogs();	
+//	}
+//	
+//	/**
+//	 * @param id
+//	 * @return
+//	 */
+//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//	public Catalog getCatalogById(@PathVariable(value = "id") Long id) {
+//   		return this.catalogService.getCatalogById(id);
+//	}
+//	
+//	/**
+//	 * create catalog
+//	 * @param catalog
+//	 * @return
+//	 */
+//	@RequestMapping(method = RequestMethod.POST)
+//	public Catalog createCatalog(@RequestBody Catalog catalog) {
+// 		return this.catalogService.createCatalog(catalog);	
+//	}
+//	
+//	/**
+//	 * @param id
+//	 * @param catalog
+//	 * @return
+//	 */
+//	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+//	public Catalog updateCatalog(@PathVariable(value = "id") Long id, @RequestBody Catalog catalog) {
+//		catalog.setId(id);
+// 		return this.catalogService.updateCatalog(catalog);
+// 	}
+//	
+//	/**
+//	 * @param id
+//	 * @return
+//	 */
+//	@RequestMapping(value = "/{id}/categories", method = RequestMethod.GET)
+//	public Collection<Category> getCategoriesByCatalogId(@PathVariable(value = "id") Long id) {
+//		Catalog catalog = this.catalogService.getCatalogById(id);
+//		return catalog.getRootCategories();
+//	}
 	
 //	@RequestMapping(value = "/{id}/categories", method = RequestMethod.POST)
 //	public Catalog addCategoriesToCatalog(@PathVariable(value = "id") Long id, Collection<Category> categories) {
@@ -82,19 +82,19 @@ public class CatalogController {
 //		return catalog;
 //	}
 	
-
-	/**
-	 * @return the catalogService
-	 */
-	public ICatalogService getCatalogService() {
-		return catalogService;
-	}
-
-	/**
-	 * @param catalogService the catalogService to set
-	 */
-	public void setCatalogService(ICatalogService catalogService) {
-		this.catalogService = catalogService;
-	}
+//
+//	/**
+//	 * @return the catalogService
+//	 */
+//	public ICatalogService getCatalogService() {
+//		return catalogService;
+//	}
+//
+//	/**
+//	 * @param catalogService the catalogService to set
+//	 */
+//	public void setCatalogService(ICatalogService catalogService) {
+//		this.catalogService = catalogService;
+//	}
 
 }
--- src/main/java/com/agh/controllers/CategoryController.java
+++ src/main/java/com/agh/controllers/CategoryController.java
@@ -13,61 +13,61 @@ import org.springframework.web.bind.annotation.RestController;
 import com.agh.domain.Category;
 import com.agh.services.ICategoryService;
 
-@RestController @RequestMapping(value ="/categories")
+//@RestController @RequestMapping(value ="/categories")
 public class CategoryController {
 	
-	@Resource
-	private ICategoryService categoryService;
-	
-	/**
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public Collection<Category> getAllCategories() {
- 	  	return this.categoryService.getAllCategoris();
-	}
-	
-	/**
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Category getCatalogById(@PathVariable(value = "id") Long id) {
-   		return this.categoryService.getCategoryById(id);
-	}
-	
-	/**
-	 * @param category
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public Category createCategory(@RequestBody Category category) {
- 		return this.categoryService.createCategory(category);	
-	}
-	
-	/**
-	 * @param id
-	 * @param category
-	 * @return
-	 */
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public Category updatecategory(@PathVariable(value = "id") Long id, @RequestBody Category category) {
-		category.setId(id);
- 		return this.categoryService.updateCategory(category);
- 	}
-	
-	/**
-	 * @return the categoryService
-	 */
-	public ICategoryService getCategoryService() {
-		return categoryService;
-	}
-
-	/**
-	 * @param categoryService the categoryService to set
-	 */
-	public void setCategoryService(ICategoryService categoryService) {
-		this.categoryService = categoryService;
-	}
+//	@Resource
+//	private ICategoryService categoryService;
+//	
+//	/**
+//	 * @return
+//	 */
+//	@RequestMapping(method = RequestMethod.GET)
+//	public Collection<Category> getAllCategories() {
+// 	  	return this.categoryService.getAllCategoris();
+//	}
+//	
+//	/**
+//	 * @param id
+//	 * @return
+//	 */
+//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//	public Category getCatalogById(@PathVariable(value = "id") Long id) {
+//   		return this.categoryService.getCategoryById(id);
+//	}
+//	
+//	/**
+//	 * @param category
+//	 * @return
+//	 */
+//	@RequestMapping(method = RequestMethod.POST)
+//	public Category createCategory(@RequestBody Category category) {
+// 		return this.categoryService.createCategory(category);	
+//	}
+//	
+//	/**
+//	 * @param id
+//	 * @param category
+//	 * @return
+//	 */
+//	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+//	public Category updatecategory(@PathVariable(value = "id") Long id, @RequestBody Category category) {
+//		category.setId(id);
+// 		return this.categoryService.updateCategory(category);
+// 	}
+//	
+//	/**
+//	 * @return the categoryService
+//	 */
+//	public ICategoryService getCategoryService() {
+//		return categoryService;
+//	}
+//
+//	/**
+//	 * @param categoryService the categoryService to set
+//	 */
+//	public void setCategoryService(ICategoryService categoryService) {
+//		this.categoryService = categoryService;
+//	}
 	
 }
